e373194e18b49745cfeffe831766fce3e7d0dd9b
Chris Silva <simpleurbanart@gmail.com>
2022-10-04 12:47:53 -0700

    testing pyscript

---
+++ myscript.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+import warnings
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# Create a visual test for numpy and pandas in pyscript
+pd.util.testing.makeMissingDataframe()
